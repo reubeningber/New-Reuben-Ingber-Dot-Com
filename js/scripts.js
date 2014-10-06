f13bd9a4b593c38b1af0ecb39417f2de144ea3a0
Reuben Ingber <reuben.ingber@expandtheroom.com>
2014-10-06 11:59:19 -0400

    New homepage still needs integration with site leaf data

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,17 @@ $(document).ready(function() {
 		e.preventDefault();
 		$('.global-nav').toggleClass('open');
 	});
+
+    // InstaFeed 
+    var feed = new Instafeed({
+        get: 'user',
+        clientId: '4bbfbc0356b24be5bb42f8bebcf178a9',
+        accessToken: '1920699.467ede5.6830e1f8ba9345378031eefce7eaa5c7',
+        userId: 1920699, 
+        template: '<li><a href="{{link}}" target="_blank"><img src="http://{{image}}" alt=""></a></li>',
+        resolution: 'thumbnail',
+        limit: 7
+    });
+    feed.run();
+
 });
